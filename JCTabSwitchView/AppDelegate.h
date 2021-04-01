6cae7ed8618ae5d9f3f5040d130db079ce9dbc51
JuneCheng <755038283@qq.com>
2021-04-01 10:08:27 +0800

    Initial Commit

---
+++ JCTabSwitchView/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  JCTabSwitchView
+//
+//  Created by JuneCheng on 2021/4/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JCTabSwitchView/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  JCTabSwitchView
+//
+//  Created by JuneCheng on 2021/4/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
